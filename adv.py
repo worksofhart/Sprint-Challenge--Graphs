78fb3cb33631a3e4d6b8c4d55fe28f10346c7d00
Michael Hart <48599443+worksofhart@users.noreply.github.com>
2020-08-30 22:39:29 -0400

    MVP + stretch (#1)
    * Add room, connect rooms, get unvisited rooms
    
    * Wander randomly without regard to avoiding previously visited rooms
    
    * Traversal working
    
    * MVP complete. Implemented functionality to automatically search for better path in attempt at stretch
    
    * Added bias toward taking turns rather than continuing straight. Stretch target reached: 956 moves with seed 22902
    
    * Cleanup, better comments
    
    * Tests now validate every record-breaking run
    
    * Adjusted turn bias to 50% of the time
    
    * Minor tweaks to how often turns are made. Current shortest traveral 948 moves with seed 386839

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Graph
 
 import random
 from ast import literal_eval
@@ -15,21 +16,105 @@ world = World()
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
-
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
 
-player = Player(world.starting_room)
+# Seed for the pseudorandom number generator
+# Best so far: 386839, number of moves 948
+seed = 386839
+shortest_traversal = 20000
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+# Find random traversals endlessly, incrementing seed and looking for better ones
+while True:
+    if not seed % 1000:  # Progress indicator
+        print(f"{seed}", end="\r", flush=True)
 
+    # Seed the random number generator for reproducible results
+    random.seed(seed)
 
+    # Start player in the first room
+    player = Player(world.starting_room)
+
+    # Will be filled with directions to walk
+    traversal_path = []
+
+    # Keep track of visited rooms so we know when we've visited them all
+    visited_rooms = set()
+    visited_rooms.add(player.current_room)
+
+    # Initialize a graph to track rooms and connections between them
+    graph = Graph()
+    graph.add_room(player.current_room.id, player.current_room.get_exits())
+
+    backtracked = False  # Track whether player has returned from a dead end
+
+    # Loop until all rooms have been visited
+    while len(visited_rooms) != len(room_graph):
+        # Get a list of unvisited exits from the current location
+        exits = graph.get_connected_rooms(
+            player.current_room.id, visited=False)
+
+        # If there are exits
+        if exits:
+            # Store current room for connecting to the next room
+            current_room = player.current_room
+
+            # If we didn't backtrack, bias toward making turns rather than going straight
+            if not backtracked:
+                # If there's more than one exit, remove the direction player came from
+                # In order to force a turn
+                if len(exits) > 1 and len(traversal_path) > 0:
+                    prev_dir = traversal_path[-1]
+                    # 30% of the time, don't continue straight
+                    if prev_dir in exits and random.random() > 0.7:
+                        exits.remove(prev_dir)
+                    direction = random.choice(exits)
+                else:
+                    direction = exits[0]
+            else:
+                # If we backtracked from a dead end, take next avail clockwise turn
+                direction = exits[0]
+                backtracked = False
+            player.travel(direction)
+            traversal_path.append(direction)
+            visited_rooms.add(player.current_room)
+            graph.add_room(player.current_room.id,
+                           player.current_room.get_exits())
+            graph.connect_rooms(
+                current_room.id, player.current_room.id, direction)
+        else:
+            route = graph.bfs(player.current_room.id)
+            for direction in route:
+                player.travel(direction)
+                traversal_path.append(direction)
+            backtracked = True
+
+    if len(traversal_path) < shortest_traversal:
+        print(f"NEW RECORD FOUND WITH SEED {seed}")
+        shortest_traversal = len(traversal_path)
+
+        # TRAVERSAL TEST
+        visited_rooms = set()
+        player.current_room = world.starting_room
+        visited_rooms.add(player.current_room)
+
+        for move in traversal_path:
+            player.travel(move)
+            visited_rooms.add(player.current_room)
+
+        if len(visited_rooms) == len(room_graph):
+            print(
+                f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+        else:
+            print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+            print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+        print(f"TOTAL MOVES: {len(traversal_path)}")
+
+    seed += 1
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,22 +126,9 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+print(f"TOTAL MOVES: {len(traversal_path)}")
---
+++ util.py
@@ -0,0 +1,103 @@
+
+# Note: This Queue class is sub-optimal. Why?
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Graph:
+
+    """Represent a graph as a dictionary of rooms mapping labels to doors."""
+
+    def __init__(self):
+        self.rooms = {}
+
+    def add_room(self, room_id, exits):
+        """
+        Add a room to the graph.
+        """
+        if not room_id in self.rooms:
+            self.rooms[room_id] = {x: '?' for x in exits}
+
+    def connect_rooms(self, room1_id, room2_id, direction):
+        """
+        Add a directed edge to the graph.
+        """
+        reverse_dir = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}[direction]
+
+        if room1_id in self.rooms and room2_id in self.rooms:
+            self.rooms[room1_id][direction] = room2_id
+            self.rooms[room2_id][reverse_dir] = room1_id
+        else:
+            raise IndexError("That room does not exist.")
+
+    def get_connected_rooms(self, room_id, visited=False):
+        """
+        Get exits from a room.
+        """
+        if visited == True:  # return only previously visited neighboring rooms
+            return [exit for exit in self.rooms[room_id] if self.rooms[room_id][exit] != '?']
+        # return unvisited neighboring rooms
+        return [exit for exit in self.rooms[room_id] if self.rooms[room_id][exit] == '?']
+
+    def path_to_directions(self, path):
+        traversal = []
+        current_room = path.pop(0)
+        while len(path) > 0:
+            next_room = path.pop(0)
+            reverse_keys = {value: key for key,
+                            value in self.rooms[current_room].items()}
+            traversal.append(reverse_keys[next_room])
+            current_room = next_room
+        return traversal
+
+    def bfs(self, starting_room):
+        """
+        Return a list containing the shortest path from
+        starting_room to destination_room in
+        breadth-first order.
+        """
+
+        # Create an empty queue and enqueue the starting room ID
+        q = Queue()
+        q.enqueue([starting_room])
+
+        # Create an empty Set to store visited rooms
+        visited = set()
+
+        # While the queue is not empty...
+        while q.size():
+            # Dequeue the first path
+            path = q.dequeue()
+            # Look at the last room in the path...
+            current_room = path[-1]
+            # And if we've found a room with an unopened door, return our path to that room
+            if '?' in self.rooms[current_room].values():
+                # Return path as directions
+                return self.path_to_directions(path)
+
+            # If the room has not been visited
+            if current_room not in visited:
+                # Mark it as visited
+                visited.add(current_room)
+                # Add a path to each room to the queue
+                for room in self.get_connected_rooms(current_room, visited=True):
+                    new_path = path.copy()
+                    new_path.append(self.rooms[current_room][room])
+                    q.enqueue(new_path)
+
+        return None
